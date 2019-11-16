95fe9b8ed035299d07ee788f9c6ff2d0e723e581
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2019-11-15 20:57:07 -0600

    Fixed decoder output

--- TestBench.v
+++ TestBench.v
@@ -23,6 +23,7 @@ module testbench();
 		#1
 		forever begin
 		#5
+			$display("Curr Max: %b -- Decoder out: %b -- LargestDFF: %b", dt.currMax, dt.decOut, dt.largest.out);
 			$display("N1:%8b  N2:%8b\nE1:%8b  E2:%8b\nS1:%8b  S2:%8b\nW1:%8b  W2:%8b\nclk: %1b\nlights(NNEESSWW):%8b\n-----------------------------------",
 															carCounts[0], carCounts[1], 
 															carCounts[2], carCounts[3],
@@ -41,14 +42,26 @@ module testbench();
 		carCounts[4] = 8'b00000000;carCounts[5] = 8'b00000000;
 		carCounts[6] = 8'b00000000;carCounts[7] = 8'b00000000;
 		#10
-		carCounts[1] = 8'b00000011;
-		carCounts[2] = 8'b00000100;
+		carCounts[0] = 8'b00000100;carCounts[1] = 8'b00000000;
+		carCounts[2] = 8'b00000000;carCounts[3] = 8'b00000000;
+		carCounts[4] = 8'b00000000;carCounts[5] = 8'b00000000;
+		carCounts[6] = 8'b00000000;carCounts[7] = 8'b00000000;
 		#10
-		carCounts[6] = 8'b00011011;
-		carCounts[7] = 8'b00000001;
+		carCounts[0] = 8'b00000000;carCounts[1] = 8'b00000000;
+		carCounts[2] = 8'b00010000;carCounts[3] = 8'b00000000;
+		carCounts[4] = 8'b00000000;carCounts[5] = 8'b00000000;
+		carCounts[6] = 8'b00000000;carCounts[7] = 8'b00000000;
 		#10
+		carCounts[0] = 8'b00000000;carCounts[1] = 8'b00000000;
+		carCounts[2] = 8'b00000000;carCounts[3] = 8'b00000000;
+		carCounts[4] = 8'b00100000;carCounts[5] = 8'b00000000;
+		carCounts[6] = 8'b00000000;carCounts[7] = 8'b00000000;
 		#10
-		
+		carCounts[0] = 8'b00000000;carCounts[1] = 8'b00000000;
+		carCounts[2] = 8'b00000000;carCounts[3] = 8'b00000000;
+		carCounts[4] = 8'b00000000;carCounts[5] = 8'b00000000;
+		carCounts[6] = 8'b01000000;carCounts[7] = 8'b00000000;
+		#20
 		
 		
 		$finish;
--- run.bat
+++ run.bat
@@ -1,6 +1,9 @@
 
 @echo off
 cls
+@echo.
+@echo ------ NEW RUN -----
+@echo.
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
