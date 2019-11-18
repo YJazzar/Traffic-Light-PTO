771ef1e95244e217abd76d5f879c7836b59764f4
lEmanl <emanuelurbina@live.com>
2019-11-18 15:23:59 -0600

    added some stuff to the traffic mode module

---
+++ src/TrafficMode.v
@@ -0,0 +1,28 @@
+//=============================================
+// D Flip-Flop
+//=============================================
+module DFF #(parameter REGISTER_WIDTH = 1) (clk,in,out);
+  input  clk;
+  input  [REGISTER_WIDTH-1:0]  in;
+  output [REGISTER_WIDTH-1:0] out;
+  reg    [REGISTER_WIDTH-1:0] out;
+  
+  always @(posedge clk)//<--This is the statement that makes the circuit behave with TIME
+    out = in;
+ endmodule
+
+module TrafficMode (clk, timeSignal, pedSignal, emgSignal, trafficModeOutput);
+
+    //  INPUT
+    input clk;
+    input timeSignal, pedSignal, emgSignal;
+    output [1:0] trafficModeOutput;
+
+    //  LOCAL VARIABLES
+    wire [1:0] nextState;
+
+    //assign nextState = 
+
+    DFF #(2) trafficMode(clk, ,trafficModeOutput);
+    
+endmodule
