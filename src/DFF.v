ba82f095ec7cedf6595054e336d114db1fd553cb
Jacob <jdg180001@utdallas.edu>
2019-11-20 16:57:23 -0600

    untested, but possibly more flexibility initializing a DFF with a parameter than SpecialDFF setting to 1 always.

--- src/DFF.v
+++ src/DFF.v
@@ -24,3 +24,15 @@ module SpecialDFF #(parameter REGISTER_WIDTH = 1) (clk,in,out);
   always @(posedge clk)//<--This is the statement that makes the circuit behave with TIME
     out = in;
 endmodule
+
+module InitializedDFF #(parameter REGISTER_WIDTH = 1) (clk,initialValue,in,out);
+  input  clk;
+  input  [REGISTER_WIDTH-1:0]  in;
+  output [REGISTER_WIDTH-1:0] out;
+  reg    [REGISTER_WIDTH-1:0] out;
+  
+  initial out = initialValue;
+
+  always @(posedge clk)//<--This is the statement that makes the circuit behave with TIME
+    out = in;
+endmodule
