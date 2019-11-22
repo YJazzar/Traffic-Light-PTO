73ec5243d72f059c2ae0825ce31d59af6b08bb64
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2019-11-22 11:55:51 -0600

    moved testbench folders

---
+++ src/Testbench Folder/TestBench.DayTime.v
@@ -0,0 +1,168 @@
+// // // module TrafficModeTestBench();
+
+// // // 	//clock signal
+// // // 	reg clk;
+// // // 	initial begin
+// // // 		forever begin
+// // // 			#5 
+// // // 			clk = 0 ;
+// // // 			#5
+// // // 			clk = 1 ;
+// // // 		end
+// // //     end
+	
+	
+// // // 	//registers to/from daytime
+// // //     reg  [7:0][7:0] carCounts; //same as reg [7:0] carCounts[0:7]
+// // //     wire [7:0] laneOutput;
+  
+// // // 	// Outside inputs used for TrafficMode
+// // // 	reg pedSignal, emgSignal, dayTime;
+
+// // // 	// Output of Traffic Mode
+// // // 	wire [1:0] currentState;	
+
+
+// // // 	TrafficMode tf (clk, pedSignal, emgSignal, dayTime, currentState);
+	
+// // // 	//display output
+// // //     initial begin
+// // // 		#1
+// // // 		forever begin
+// // // 		#5
+// // // 			$display("clk: %b ------- currentState: %2b", clk, currentState);
+// // // 			$display("pedSignal = %1b", pedSignal);
+// // // 			$display("emgSignal = %1b", emgSignal);
+// // // 			$display("  dayTime = %1b", dayTime);
+// // // 			$display("---------------------------------");
+// // // 		end														
+// // //     end
+	
+// // // 	//input stimulus
+// // // 	initial begin
+// // // 		#2
+// // // 		pedSignal = 1'b0;
+// // // 		emgSignal = 1'b0;
+// // // 		  dayTime = 1'b0;
+// // // 		#10
+// // // 		pedSignal = 1'b0;
+// // // 		emgSignal = 1'b0;
+// // // 		  dayTime = 1'b1;
+// // // 		#10
+// // // 		pedSignal = 1'b0;
+// // // 		emgSignal = 1'b1;
+// // // 		  dayTime = 1'b0;
+// // // 		#10
+// // // 		pedSignal = 1'b0;
+// // // 		emgSignal = 1'b1;
+// // // 		  dayTime = 1'b1;
+// // // 		#10
+// // // 		pedSignal = 1'b1;
+// // // 		emgSignal = 1'b0;
+// // // 		  dayTime = 1'b0;
+// // // 		#10
+// // // 		pedSignal = 1'b1;
+// // // 		emgSignal = 1'b0;
+// // // 		  dayTime = 1'b0;
+// // // 		#10
+// // // 		pedSignal = 1'b1;
+// // // 		emgSignal = 1'b0;
+// // // 		  dayTime = 1'b1;
+// // // 		#10
+// // // 		pedSignal = 1'b1;
+// // // 		emgSignal = 1'b1;
+// // // 		  dayTime = 1'b0;
+// // // 		#10
+// // // 		pedSignal = 1'b1;
+// // // 		emgSignal = 1'b1;
+// // // 		  dayTime = 1'b1;
+// // // 		#10
+// // // 		$finish;
+// // // 	end
+
+
+
+// // // endmodule
+
+// module DayTimeTestBench();
+// 	//clock signal
+// 	reg clk;
+// 	reg rst;
+	
+// 	initial begin
+// 		forever begin
+// 			#5 
+// 			clk = 0 ;
+// 			#5
+// 			clk = 1 ;
+// 		end
+//     end
+
+// 	initial begin 
+// 		rst = 1;
+// 		#10
+// 		rst = 0;
+// 	end
+	
+	
+// 	//registers to/from daytime
+//     reg  [7:0][7:0] carCounts; //same as reg [7:0] carCounts[0:7]
+//     wire [7:0] laneOutput;
+//     wire  [6:0] loadTimer;
+//     DayTime dt(clk, 1'b0,  carCounts, laneOutput, loadTimer);
+// 	// (clk, isZero, lane, laneOutput, loadTimer);
+// 	integer f;
+
+// 	//display output
+//     initial begin
+// 		f = $fopen("output-DayTime.txt","w");
+// 		$fwrite(f, "-----------------------------------\n");
+// 		#1
+// 		forever begin
+// 		#5
+// 			$fwrite(f, "N1:%8d  N2:%8d\nE1:%8d  E2:%8d\nS1:%8d  S2:%8d\nW1:%8d  W2:%8d\nclk: %1b\nlights(WWSSEENN):%8b\n-----------------------------------\n",
+// 															carCounts[0], carCounts[1], 
+// 															carCounts[2], carCounts[3],
+// 															carCounts[4], carCounts[5],
+// 															carCounts[6], carCounts[7],
+// 															clk,
+// 															laneOutput);
+											
+// 		end			
+// 		$fclose(f);											
+//     end
+	
+// 	//input stimulus (changing number of cars)
+// 	initial begin
+// 		#2
+// 		carCounts[0] = 8'b00000100;carCounts[1] = 8'b00000000;
+// 		carCounts[2] = 8'b00001000;carCounts[3] = 8'b00000000;
+// 		carCounts[4] = 8'b00001000;carCounts[5] = 8'b00000100;
+// 		carCounts[6] = 8'b00000010;carCounts[7] = 8'b10000000;
+// 		#10
+// 		carCounts[0] = 8'b00000100;carCounts[1] = 8'b00000000;
+// 		carCounts[2] = 8'b00001000;carCounts[3] = 8'b00000000;
+// 		carCounts[4] = 8'b00001000;carCounts[5] = 8'b00000100;
+// 		carCounts[6] = 8'b00000010;carCounts[7] = 8'b10000000;
+// 		#10
+// 		carCounts[0] = 8'b00000100;carCounts[1] = 8'b00000000;
+// 		carCounts[2] = 8'b00001000;carCounts[3] = 8'b00000000;
+// 		carCounts[4] = 8'b00001000;carCounts[5] = 8'b10000100;
+// 		carCounts[6] = 8'b00000010;carCounts[7] = 8'b10000000;
+// 		#10
+// 		carCounts[0] = 8'b00000100;carCounts[1] = 8'b00000000;
+// 		carCounts[2] = 8'b00001000;carCounts[3] = 8'b00000000;
+// 		carCounts[4] = 8'b00001000;carCounts[5] = 8'b00000100;
+// 		carCounts[6] = 8'b00000010;carCounts[7] = 8'b00000000;
+// 		#10
+// 		carCounts[0] = 8'b10000100;carCounts[1] = 8'b10000000;
+// 		carCounts[2] = 8'b01001000;carCounts[3] = 8'b10100000;
+// 		carCounts[4] = 8'b00001000;carCounts[5] = 8'b10000100;
+// 		carCounts[6] = 8'b00000010;carCounts[7] = 8'b10000000;
+// 		#20
+		
+// 		$finish;
+// 	end
+
+// endmodule
+
---
+++ src/Testbench Folder/TestBench.test.v
@@ -0,0 +1,146 @@
+/**********************************************
+    THESE ARE TESTS FOR THE TEST BENCH
+***********************************************/
+
+module TestBreadboard (clk, rst, timeSignal, pedSignal, emgSignal, emgLane);
+    //  INPUT
+    input clk, rst;
+    input timeSignal, pedSignal, emgSignal;
+    input [7:0] emgLane;
+
+    //  LOCAL VARIABLES
+    wire [1:0] trafficMode;
+    wire [3:0] trafficModeOneHot;
+    wire [7:0] walkingLightOutput;
+    wire [7:0] emergencyLightOutput;
+    wire [7:0] pedestrianLightOutput;
+    wire [7:0] nightTimeLightOutput;
+    wire [7:0] dayTimeLightOutput;
+    //  LOCA VARIABLE TRAFFIC LIGHT OUTPUT
+    wire [7:0] trafficLightOutput;
+
+    //  TRAFFIC MODE MODULES
+    Pedestrian PedestrianModule (pedSignal, walkingLightOutput, pedestrianLightOutput);
+    Emergency EmergencyModule (emgLane, emergencyLightOutput);
+    assign nightTimeLightOutput = 8'b00000000;
+    assign dayTimeLightOutput = 8'b00000000;
+    // NightTime NightTimeModule (clk, rst, nightTimeLightOutput);
+
+    //  TRAFFIC MODE STATE MACHINE
+    TrafficMode TM (clk, rst, timeSignal, pedSignal, emgSignal, trafficMode);
+
+    //  DECODE TRAFFIC MODE
+    Decoder TrafficModeDecoder (trafficMode, trafficModeOneHot);
+
+    //  SELECT MODULE OUTPUT TO USE
+    Mux4 #(8) LightOutputMux (emergencyLightOutput, pedestrianLightOutput, nightTimeLightOutput, dayTimeLightOutput, trafficModeOneHot, trafficLightOutput);
+endmodule
+
+module TestTestBench ();
+    integer f;  // File 
+    //  SIGNALS
+    reg clk, rst;
+    reg timeSignal, pedSignal, emgSignal;
+
+    //  TRAFFIC LANE COUNT
+    reg [7:0] n1;
+    reg [7:0] n2;
+    reg [7:0] e1;
+    reg [7:0] e2;
+    reg [7:0] s1;
+    reg [7:0] s2;
+    reg [7:0] w1;
+    reg [7:0] w2;
+
+    //  WHICH LANE THE EMERGENCY IS IN
+    reg [7:0] emgLane;
+
+    //  BREADBOARD
+    TestBreadboard TB(clk, rst, timeSignal, pedSignal, emgSignal, emgLane);
+
+    //  THREAD WITH CLOCK CONTROL
+    initial
+    begin
+        forever
+            begin
+                #5
+                clk = 0;
+                #5
+                clk = 1;
+            end 
+    end
+
+    //  THREAD THAT DISPLAYS OUTPUTS
+    initial
+    begin
+        f = $fopen("Final-Output.txt", "w");
+        #2
+        forever
+            begin
+                #5
+                $fwrite(f, "---+---+---+-----+---+---+----+--------+----+---+---+---+---+---+----+---+---+---\n");
+                $fwrite(f, "---+---+---+-----+---+---+----+--------+----+---+---+---+---+---+----+---+---+---\n");
+                $fwrite(f, "                                                              \n");
+                $fwrite(f, "                            N                                 \n");
+                $fwrite(f, "            S1: %8b                                           \n", s1);
+                $fwrite(f, "            S2: %8b                                           \n", s2);
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                %1b%1b|   |   |###| %1b | %1b |%1b%1b         \n", TB.walkingLightOutput[6], TB.walkingLightOutput[0], TB.trafficLightOutput[0], TB.trafficLightOutput[1], TB.walkingLightOutput[4], TB.walkingLightOutput[7]);
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "                 %1b                     W1: %8b              \n", TB.trafficLightOutput[6], w1);
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "                 %1b                     W2: %8b              \n", TB.trafficLightOutput[7], w2);
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "                  |   |   |###|       |                       \n");
+                $fwrite(f, "  W               |   |   |###|       |                E      \n");
+                $fwrite(f, "                  |   |   |###|       |                       \n");
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "     E1: %8b                      %1b                         \n", e1, TB.trafficLightOutput[2]);
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "     E2: %8b                      %1b                         \n", e2, TB.trafficLightOutput[3]);
+                $fwrite(f, "     -------------         ---         -------------          \n");
+                $fwrite(f, "                %1b%1b| %1b | %1b |###|   |   |%1b%1b         \n", TB.walkingLightOutput[2], TB.walkingLightOutput[1], TB.trafficLightOutput[4], TB.trafficLightOutput[5], TB.walkingLightOutput[5], TB.walkingLightOutput[3]);
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                  |   |   |###|   |   |                       \n");
+                $fwrite(f, "                                 N1: %8b                      \n", n1);
+                $fwrite(f, "                                 N2: %8b                      \n", n2);
+                $fwrite(f, "                            S                                 \n");
+                $fwrite(f, "                                                              \n");
+                $fwrite(f, "---+---+---+-----+---+---+----+--------+----+---+---+---+---+---+----+---+---+---\n");
+                $fwrite(f, "CLK|RST|DAY|NIGHT|PED|EMG|MODE|EMG_LANE\n");
+                $fwrite(f, "---+---+---+-----+---+---+----+--------\n");
+                $fwrite(f, "%1b  |%1b  |%1b   |%1b   |%1b  |%1b  |%2b  |%8b\n", clk, rst, ~rst, timeSignal, pedSignal, emgSignal, TB.trafficMode, emgLane);
+                
+            end
+    end
+
+    //  THREAD WITH INPUT STIMULUS
+    initial
+    begin
+        #2
+        #5 rst = 1; timeSignal = 0; pedSignal = 0; emgSignal = 0;
+        #5 $fwrite(f, "+--------------+\n");
+           $fwrite(f, "| RESET SIGNAL |\n");
+           $fwrite(f, "+--------------+\n");
+        #5 rst = 0; timeSignal = 0; pedSignal = 0; emgSignal = 1;
+           w1 = 8'b00000000; w2 = 8'b00000000; s1 = 8'b00000000; s2 = 8'b00000000;
+           e1 = 8'b00000000; e2 = 8'b00000000; n1 = 8'b10000000; n2 = 8'b00000000;
+           emgLane = 8'b00001000;
+        #5 $fwrite(f, "+------------------+\n");
+           $fwrite(f, "| EMERGENCY SIGNAL |\n");
+           $fwrite(f, "+------------------+\n");
+        #5 rst = 0; timeSignal = 0; pedSignal = 1; emgSignal = 0;
+           w1 = 8'b00000000; w2 = 8'b00000000; s1 = 8'b00000000; s2 = 8'b00000000;
+           e1 = 8'b00000000; e2 = 8'b00000000; n1 = 8'b10000000; n2 = 8'b00000000;
+           emgLane = 8'b00001000;
+        #5 $fwrite(f, "+-------------------+\n");
+           $fwrite(f, "| PEDESTRIAN SIGNAL |\n");
+           $fwrite(f, "+-------------------+\n");
+           
+            $fclose(f);
+        $finish;
+    end
+endmodule
