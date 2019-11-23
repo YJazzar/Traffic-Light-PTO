ecdd9d8c8e63f9bc1931a47c6ca0bdd888f8861c
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2019-11-22 21:47:28 -0600

    Changed emergency and pedestrian labeling

--- src/Emergency.v
+++ src/Emergency.v
@@ -1,14 +1,14 @@
-//module Emergency(emergencyLane, laneOutput, loadCommand, loadTime);
-module Emergency(emergencyLane, laneOutput, loadTime);
+//module Emergency(emergencyLane, emergencyLightOutput, loadCommand, loadTime);
+module Emergency(emergencyLane, emergencyLightOutput, loadTime);
     //  INPUTS
     input [0:7] emergencyLane;
 
     //  OUTPUTS
-    output [0:7] laneOutput;
+    output [0:7] emergencyLightOutput;
     output [6:0] loadTime;
     //output loadCommand;
 
-    assign laneOutput = {{emergencyLane[0] | emergencyLane[1]}, {emergencyLane[0] | emergencyLane[1]},
+    assign emergencyLightOutput = {{emergencyLane[0] | emergencyLane[1]}, {emergencyLane[0] | emergencyLane[1]},
                          {emergencyLane[2] | emergencyLane[3]}, {emergencyLane[2] | emergencyLane[3]},
                          {emergencyLane[4] | emergencyLane[5]}, {emergencyLane[4] | emergencyLane[5]},
                          {emergencyLane[6] | emergencyLane[7]}, {emergencyLane[6] | emergencyLane[7]}};
--- src/Pedestrian.v
+++ src/Pedestrian.v
@@ -1,13 +1,13 @@
-module Pedestrian(pedSignal, walkingLightOutput, laneOutput, loadTime);
+module Pedestrian(pedSignal, walkingLightOutput, nightTimeLightOutput, loadTime);
     //  INPUTS
     input pedSignal;
 
     //  OUTPUTS
-    output [0:7] laneOutput;
+    output [0:7] nightTimeLightOutput;
     output [0:7] walkingLightOutput;
     output [6:0] loadTime;
 
-    assign laneOutput = 8'b00000000;
+    assign nightTimeLightOutput = 8'b00000000;
 
     Mux2 #(8) PedestrianLightMux ({8'b11111111}, {8'b00000000}, {pedSignal, ~pedSignal}, walkingLightOutput);
 
